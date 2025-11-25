90270c3835c574db0c72588588e521dd144443a9
Felix Li <fli5@academic.rrc.ca>
2025-11-25 16:46:35 -0600

    Update specPattern in Cypress config to include full path

--- test/cypress/cypress.config.js
+++ test/cypress/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'http://localhost:3000',
-        specPattern: 'e2e/**/*.cy.{js,ts}',
+        specPattern: 'test/cypress/e2e/**/*.cy.{js,ts}',
         supportFile: false,
         viewportWidth: 1280,
         viewportHeight: 720,
