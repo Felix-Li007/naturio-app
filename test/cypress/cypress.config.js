09ed13400e55531a461d96c1652fe69766307efb
Felix Li <fli5@academic.rrc.ca>
2025-11-25 17:04:44 -0600

    Add screenshots and videos folder paths to Cypress config

--- test/cypress/cypress.config.js
+++ test/cypress/cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
     e2e: {
         baseUrl: 'http://localhost:3000',
         specPattern: 'test/cypress/e2e/**/*.cy.{js,ts}',
+        screenshotsFolder: "test/cypress/screenshots",
+        videosFolder: "test/cypress/videos",
         supportFile: false,
         viewportWidth: 1280,
         viewportHeight: 720,
